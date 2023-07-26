62cd8d022cb03d54571927cd8541558ebadb083b
ZifeRRoT <ziferrot@gmail.com>
2023-07-26 14:25:39 +0300

    change browser to firefox

--- chrome.py
+++ chrome.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 def run_browser(url):
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
     driver.get(url)
-    return driver.page_source
+    page = driver.page_source
+    driver.quit()
+    return page
