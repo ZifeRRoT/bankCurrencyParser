fb2471ba94a4b66f266743b4de0751f3f481b258
ZifeRRoT <ziferrot@gmail.com>
2024-03-27 10:34:58 +0300

    fixed oschadbank

--- banks/oschadbank.py
+++ banks/oschadbank.py
@@ -1,24 +1,20 @@
-import chrome
-from bs4 import BeautifulSoup
 from config import oschadbank_url
 from tg_informer import send_error
 from database import insert_to_db
+import requests
 
 
 @send_error
 def start(header):
     result = {}
-    request = chrome.run_browser(oschadbank_url)
-    soup = BeautifulSoup(request, 'lxml')
-    course_table = soup.find("tbody", class_="heading-block-currency-rate__table-body")
-    course_tr = course_table.find_all("tr")
-    for i in course_tr:
-        all_td = i.find_all("td")
-        name = all_td[1].text
-        if name == "HUF":
-            continue
-        purchase = float(all_td[3].text)/int(all_td[2].text)
-        sale = float(all_td[4].text)/int(all_td[2].text)
-        result.update({name: {"purchase": float('{:.3f}'.format(purchase)), "sale": float('{:.3f}'.format(sale))}})
+    request = requests.get(oschadbank_url, headers=header).json()
+    items = dict(request['items'])
+    for item in items:
+        for i in items[item]:
+            result.update({
+                item: {
+                    "purchase": float('{:.3f}'.format(float(items[item][i]['rate']['buy']))),
+                    "sale": float('{:.3f}'.format(float(items[item][i]['rate']['sale'])))
+                }
+            })
     insert_to_db(300465, result)
-    # print(result)
